de1815acdd095f99648e337ee9dc26df79bb75ba
Eric <ericstein.rl@gmail.com>
2018-12-22 18:52:07 -0600

    Initial Commit

---
+++ Husle Mode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Husle Mode
+//
+//  Created by Eric Stein on 12/22/18.
+//  Copyright © 2018 Eric Stein. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
